b1078c775a0bbc8a0e33fb38d04a5e5dac43b9ce
Ryan Marshall <rmarshall9n@gmail.com>
2022-09-22 15:51:56 +0100

    Added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import Lights from './Services/Lights/Lights'
+const cors = require('cors')
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app: Express = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/lights', Lights)
 
